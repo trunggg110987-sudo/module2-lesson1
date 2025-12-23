5b2ebe45bfe2e74a507a9bfc392626c7889385cb
Trung <trung@example.com>
2025-12-23 16:07:26 +0700

    Add SayHello project

--- Sayhello.java
+++ Sayhello.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Sayhello {
+    public static void main(String[] args) {
+        System.out.println("nhap ten cua ban");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("xin chao " + name);
+    }
 }
